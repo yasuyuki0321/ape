9878027e2820adb04770564e6d6cf2e2bba52b9b
Yasuyuki Kikuchi <yasuyuki0321@gmail.com>
2023-10-29 00:52:54 +0900

    feat: configファイル管理用のメニューの追加

--- cmd/config.go
+++ cmd/config.go
@@ -1,40 +1,34 @@
 /*
 Copyright © 2023 NAME HERE <EMAIL ADDRESS>
-
 */
 package cmd
 
 import (
 	"fmt"
+	"os"
+	"path/filepath"
 
 	"github.com/spf13/cobra"
 )
 
-// configCmd represents the config command
 var configCmd = &cobra.Command{
 	Use:   "config",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
+	Short: "manage config.yaml file",
+	Run:   executeConfig,
+}
 
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("config called")
-	},
+func executeConfig(cmd *cobra.Command, args []string) {
+	configPath := filepath.Join(".config.yaml")
+
+	if configBytes, err := os.ReadFile(configPath); err == nil {
+		fmt.Println(string(configBytes))
+	} else {
+		fmt.Printf("Error reading config file: %v\n", err)
+	}
 }
 
 func init() {
 	rootCmd.AddCommand(configCmd)
 
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// configCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// configCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	configCmd.PersistentFlags().String("list", "", "A help for foo")
 }
