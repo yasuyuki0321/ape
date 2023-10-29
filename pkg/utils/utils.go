0c288c3234b9a09c98154355343d5132fdffbd28
Yasuyuki Kikuchi <yasuyuki0321@gmail.com>
2023-10-29 17:22:20 +0900

    feat: previewのメッセージ出力内容を変更

--- pkg/utils/utils.go
+++ pkg/utils/utils.go
@@ -33,17 +33,18 @@ func PrintHeader(outputBuffer *bytes.Buffer, account Account, command string) {
 
 	outputBuffer.WriteString(fmt.Sprintln(strings.Repeat("-", 10)))
 	outputBuffer.WriteString(fmt.Sprintf("Time: %v\n", time.Now().Format("2006-01-02 15:04:05")))
-	outputBuffer.WriteString(fmt.Sprintf("Account: %s (%s)\n", account.Name, accountId))
+	outputBuffer.WriteString(fmt.Sprintf("Account: %s(%s)\n", account.Name, accountId))
 	outputBuffer.WriteString(fmt.Sprintf("Command: %s\n", command))
 	outputBuffer.WriteString(fmt.Sprintln(strings.Repeat("-", 10)))
 }
 
 func DisplayPreview(accounts []Account, command string) bool {
 	fmt.Println("Accounts:")
-
 	for _, account := range accounts {
-		fmt.Printf("Name: %s command: %s\n", account.Name, command)
+		fmt.Printf("Account: %s(%s)\n", account.Name, strings.Split(account.RoleArn, ":")[4])
 	}
+
+	fmt.Printf("\nCommand: %s\n", command)
 
 	fmt.Print("\nDo you want to continue? [y/N]: ")
 	var response string
